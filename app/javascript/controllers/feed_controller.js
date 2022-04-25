5002b7450f9c3f096c4f226feb04103e8b06971d
Leonid Nazarov <work.leonid@gmail.com>
2022-04-25 23:35:43 +0300

    update add feed form

--- app/javascript/controllers/feed_controller.js
+++ app/javascript/controllers/feed_controller.js
@@ -1,8 +1,19 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["url", "errorMessage", "config", "inspiration", "preview", "empty"]
+    static targets = ["url", "errorMessage", "config", "inspiration", "preview", "empty", "formBg", "clearInput"]
 
+    reset(event) {
+        this.previewTarget.classList.add("hidden")
+        this.emptyTarget.classList.remove("hidden")
+        this.configTarget.classList.add("hidden")
+        this.errorMessageTarget.classList.add("hidden")
+        this.inspirationTarget.classList.remove("hidden")
+        this.inspirationTarget.classList.add("scale-100")
+        this.formBgTarget.classList.add("bg-indigo-500")
+
+        this.clearInputTarget.classList.add("hidden")
+    }
     check(event) {
         const element = this.urlTarget
         const url = element.value
@@ -12,6 +23,9 @@ export default class extends Controller {
         }
         else if  (url == "www") {
             this.inspirationTarget.classList.add("hidden")
+            this.formBgTarget.classList.add("bg-purple-500")
+            this.formBgTarget.classList.remove("bg-indigo-500")
+            this.formBgTarget.classList.remove("bg-red-400")
             this.errorMessageTarget.classList.add("hidden")
             this.configTarget.classList.remove("hidden")
             this.previewTarget.classList.remove("hidden")
@@ -22,6 +36,9 @@ export default class extends Controller {
             this.configTarget.classList.add("hidden")
             this.previewTarget.classList.add("hidden")
             this.emptyTarget.classList.remove("hidden")
+
+            this.formBgTarget.classList.add("bg-red-400")
+            this.formBgTarget.classList.remove("bg-indigo-500")
         } else {
             this.previewTarget.classList.add("hidden")
             this.emptyTarget.classList.remove("hidden")
@@ -29,6 +46,8 @@ export default class extends Controller {
             this.errorMessageTarget.classList.add("hidden")
             this.inspirationTarget.classList.remove("hidden")
             this.inspirationTarget.classList.add("scale-100")
+            this.formBgTarget.classList.add("bg-indigo-500")
+            this.clearInputTarget.classList.remove("hidden")
         }
     }
 
